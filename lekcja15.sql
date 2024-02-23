61abffa7042a021836dc92bc7fbd7209cfc18ed0
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-02-23 12:12:31 +0100

    lekcja15.sql

--- lekcja15.sql
+++ lekcja15.sql
@@ -36,13 +36,31 @@ FROM actor
 27.2 : 4 case
   
 SELECT first_name,
-	CASE first_name WHEN 'NICK' THEN 'Nick is here' END AS Nick,
+    CASE first_name WHEN 'NICK' THEN 'Nick is here' END AS Nick,
     CASE first_name WHEN 'ED' THEN 'Ed Edd and Eddie' END AS Ed,
     CASE first_name WHEN 'GRACE' THEN 'Holy Grace' END AS Grace,
     CASE first_name WHEN 'JOE' THEN 'You like ice cream?' END AS Joe
 FROM actor
 
+27.3 : inny warunek niz wyświetlanie
 
+SELECT 
+    COUNT(CASE first_name WHEN 'NICK' THEN 1 END) AS Nick,
+    COUNT(CASE first_name WHEN 'ED' THEN 1 END) AS Ed,
+    COUNT(CASE first_name WHEN 'GRACE' THEN 1 END) AS Grace,
+    COUNT(CASE first_name WHEN 'JOE' THEN 1 END) AS Joe
+FROM actor
+	
+27.4 : inne niż case
 
+SELECT first_name,
+    IF(first_name LIKE 'NICK','Nick is here','') as Nick,
+    IF(first_name LIKE 'ED','Ed Edd and Eddie','') as Ed,
+    IF(first_name LIKE 'GRACE','Holy Grace','') as Grace,
+    IF(first_name LIKE 'JOE','You like ice cream?','') as Joe
+FROM actor
 
+27.5 : make it pretty
+
+DONE ALREADY!!!
 
