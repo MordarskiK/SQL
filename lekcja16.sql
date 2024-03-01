3d29dd57f53f5d034621a4094669267d03d77b9c
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-03-01 12:51:47 +0100

    Update lekcja16.sql

--- lekcja16.sql
+++ lekcja16.sql
@@ -63,3 +63,59 @@ INSERT INTO sprzedaz (id_towaru,sztuk,cena,kursantki_id) VALUES(1,11,123,1);
 INSERT INTO sprzedaz (id_towaru,sztuk,cena,kursantki_id) VALUES(2,22,222,2);
 INSERT INTO sprzedaz (id_towaru,sztuk,cena,kursantki_id) VALUES(3,45,6,5);
 
+
+ALTER TABLE sprzedaz add blokada INt DEFAULT 1;
+
+DELIMITER $$
+    CREATE TRIGGER aktualizacjaSprzedazy before UPDATE ON sprzedaz
+    FOR EACH ROW
+        BEGIN 
+            IF(SELECT blokada FROM sprzedaz WHERE id=new.id)>0 THEN
+                SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Blad: nie mozna zaktualizowac rekordu';
+            END if;
+        END $$
+DELIMITER ;	
+
+
+START TRANSACTION;
+	UPDATE sprzedaz SET cena=300 WHERE id = 3;	--to nie dziala bo blokada jest na 1
+COMMIT;
+
+INSERT INTO sprzedaz(id_towaru,sztuk,cena,kursantki_id,blokada) VALUES (1,10,100,2,0)
+
+START TRANSACTION;
+	UPDATE sprzedaz SET cena=333 WHERE id = 4;	--to dzianla bo blokada to 0
+COMMIT;
+
+
+CREATE TABLE zdarzenia(
+	id serial,
+	czas timestamp,
+    wydarzenia varchar(255),
+    uzytkownik varchar(255),
+    tablica varchar(255),
+    record_id bigint
+);
+
+DELIMITER $$
+	CREATE TRIGGER losSprzedazy AFTER INSERT ON sprzedaz
+    FOR EACH ROW
+    BEGIN
+    	INSERT INTO zdarzenia(wydarzenia,uzytkownik,tablica,record_id)	--po insercie na sprzedarzy dodaje do tablicy zdarzenia odpowiednie wartosci
+        VALUES ('insert','trigger','sprzedarz',new.id);
+    END $$
+DELIMITER ;
+
+drop TRIGGER nowaSprzedaz
+
+DELIMITER $$
+	CREATE TRIGGER nowaSprzedarz AFTER INSERT ON sprzedaz
+    FOR EACH ROW
+    BEGIN
+    	UPDATE kursantki set ostatni_zakup_id=new.id WHERE kursantki_id = new.kursantki_id;
+        INSERT INTO zdarzenia(wydarzenia,uzytkownik,tablica,record_id)
+        VALUES ('insert','trigger','sprzedarz',new.id);
+    END $$
+DELIMITER ;
+
+INSERT INTO sprzedaz (id_towaru,sztuk,cena,kursantki_id,blokada) VALUES (1,555,1010,5,0)
