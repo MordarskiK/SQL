ecb7612affc65a4c2596b77c49bb382037cb7a65
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-03-08 13:52:12 +0100

    Update lekcja17.sql

--- lekcja17.sql
+++ lekcja17.sql
@@ -36,3 +36,141 @@ UPDATE memo SET zrobione = 1 WHERE memo_id = 1
 ---------------------------------------------------------------------------------------------------
 https://dev.mysql.com/doc/index-other.html   -  import world database
 
+widok to zapytanie do bazy danych zapisywane pod daną nazwą
+
+SELECT
+	GNP AS PKB,
+    name as Kraj,
+    Continent as Kontynent,
+    Population as Populacja,
+    SurfaceArea as Powierzchnia,
+    LifeExpectancy as DlugoscZycia
+FROM
+	country
+ORDER BY PKB DESC;
+
+--------------------
+CREATE VIEW PozycjaPKB AS
+SELECT
+	GNP AS PKB,
+    name as Kraj,
+    Continent as Kontynent,
+    Population as Populacja,
+    SurfaceArea as Powierzchnia,
+    LifeExpectancy as DlugoscZycia
+FROM
+	country
+ORDER BY PKB DESC;
+
+SELECT PKB,Kraj FROM pozycjapkb
+
+--------------------------	
+CREATE VIEW Miasta AS
+SELECT
+    name as Miasto,
+    CountryCode as KodKraju,
+    District as Dystrykt,
+    Population as Populacja
+FROM
+	city
+ORDER BY Populacja DESC;
+
+---------------------
+CREATE VIEW JezykKraju AS
+SELECT
+    CountryCode as KodKraju,
+    Language as Jezyk,
+    IsOfficial as JestOficjalny,
+    Percentage as Procent
+FROM
+	countrylanguage
+ORDER BY Procent DESC;
+
+--------------------------
+
+set @nr := 0;								 |  Jedno
+SELECT @nr := @nr+1 as Pozycja,PKB, Kraj, DlugoscZycia FROM pozycjapkb;	 |  zapytanie
+
+
+SELECT * FROM miasta WHERE miasta.Dystrykt='New York' ORDER BY miasta.Populacja DESC;
+
+
+SELECT 
+	CONCAT(C.first_name,' ',C.last_name) AS Klient,
+    SUM(P.amount) AS Zaplacil,
+    AVG(P.amount) AS Srednia
+FROM
+	payment as P, customer AS C
+WHERE
+	C.customer_id = P.customer_id
+GROUP BY
+	C.customer_id
+ORDER BY Zaplacil DESC;
+
+
+
+CREATE VIEW Platnosci AS
+SELECT 
+	CONCAT(C.first_name,' ',C.last_name) AS Klient,
+    SUM(P.amount) AS Zaplacil,
+    AVG(P.amount) AS Srednia
+FROM
+	payment as P, customer AS C
+WHERE
+	C.customer_id = P.customer_id
+GROUP BY
+	C.customer_id
+ORDER BY Zaplacil DESC;
+
+---------------------------------------------------------------
+A: tabela polaczona 2 tabele z world: zad C
+
+B: 3 widoki z sakili z join:
+
+CREATE VIEW sakila_stalk_customer AS
+SELECT 
+	C.first_name AS Imie,
+    C.last_name AS Nazwisko,
+    A.address AS Adres
+FROM
+	(customer AS C
+    JOIN address as A ON (C.address_id=A.address_id))
+GROUP BY C.first_name
+ORDER BY C.first_name;
+	
+C: dodac kraj do jezyka:
+
+CREATE VIEW KrajIJezyk AS
+SELECT 
+	country.Name AS Kraj, 
+    countrylanguage.Language AS Jezyk, 
+    countrylanguage.IsOfficial AS CzyOficjalny, 
+    countrylanguage.Percentage AS Procent
+FROM 
+	country 
+LEFT JOIN 
+	countrylanguage ON country.Code=countrylanguage.CountryCode
+WHERE 
+	country.Code=countrylanguage.CountryCode
+GROUP BY
+	country.Name	
+ORDER BY Procent DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
