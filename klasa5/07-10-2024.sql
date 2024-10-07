3969c57e6f2588a544faf0a0ce0e4a021b7c3d7a
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-10-07 10:28:41 +0200

    Update and rename 30-09-2024.sql to 07-10-2024.sql

--- klasa5/07-10-2024.sql
+++ klasa5/07-10-2024.sql
@@ -77,16 +77,139 @@ HAVING COUNT(dane_wypozyczen.id_wypozyczenia) > 1) t1
     
 5.	Wyświetl informacje o klientach (imię i nazwisko), którzy dokonali największej liczby wypożyczeń.
 
-SELECT klienci.id_klienta, klienci.imie_klienta, klienci.nazwisko_klienta FROM klienci
-WHERE klienci.id_klienta in (SELECT MAX(COUNT(wypozyczenia.id_klienta)),klienci.id_klienta FROM wypozyczenia
-                         JOIN klienci ON klienci.id_klienta = wypozyczenia.id_klienta)
+SELECT klienci.id_klienta, klienci.imie_klienta, klienci.nazwisko_klienta, COUNT(wypozyczenia.id_klienta) FROM klienci
+JOIN wypozyczenia ON wypozyczenia.id_klienta = klienci.id_klienta
+GROUP BY klienci.id_klienta, klienci.imie_klienta, klienci.nazwisko_klienta
+HAVING COUNT(wypozyczenia.id_klienta) = (
+	SELECT COUNT(wypozyczenia.id_klienta) FROM wypozyczenia
+    GROUP BY wypozyczenia.id_klienta
+    ORDER BY COUNT(wypozyczenia.id_klienta) DESC
+    LIMIT 1
+);
     
 6.	Wyświetl informacje o klientach (imię i nazwisko), którzy nigdy nie wypożyczali aut z klasy 'sedan'.
+
+SELECT
+    klienci.id_klienta,
+    klienci.imie_klienta,
+    klienci.nazwisko_klienta
+FROM
+    klienci
+WHERE
+    id_klienta NOT IN(
+    SELECT
+        wypozyczenia.id_klienta
+    FROM
+        wypozyczenia
+    JOIN dane_wypozyczen 
+    	ON dane_wypozyczen.id_wypozyczenia = wypozyczenia.id_wypozyczenia
+    JOIN samochody 
+        ON samochody.id_samochodu = dane_wypozyczen.id_samochodu
+    JOIN klasy_samochodow 
+        ON klasy_samochodow.id_klasy = samochody.id_klasy
+    WHERE
+        klasy_samochodow.nazwa_klasy = "sedan"
+    GROUP BY
+        wypozyczenia.id_klienta
+);
+	
 7.      Wyświetl wszystkie marki i modele samochodów (wraz z id i nr rejestracyjnym) z dopiskiem czy były wypożyczane w okresie od 21 do 31 grudnia 2022.
+
+SELECT samochody.id_samochodu, samochody.marka, samochody.model, samochody.nr_rejestracyjny, 
+	(CASE WHEN id_samochodu IN (
+    SELECT id_samochodu FROM dane_wypozyczen
+    JOIN wypozyczenia ON wypozyczenia.id_wypozyczenia = dane_wypozyczen.id_wypozyczenia
+    WHERE wypozyczenia.data_wyp BETWEEN "2022-12-21" and "2022-12-31") THEN 'tak' ELSE 'nie' END) as 'wyp nie wyp'
+FROM samochody;
+
 8.	Wyświetl informacje o samochodach, których cena wypożyczenia za dobę jest większa lub równa średniej cenie samochodu w danej klasie.
-9.      Wyświetl informacje o samochodach, których cena wypożyczenia za dobę jest największa w danej klasie. 
+
+SELECT
+    samochody.id_samochodu,
+    samochody.marka,
+    samochody.model,
+    round(AVG(samochody.cena_katalogowa),2),
+    dane_wypozyczen.cena_doba,
+   	klasy_samochodow.nazwa_klasy
+FROM
+    samochody 												/*WIP - Work in progress*/
+JOIN dane_wypozyczen ON dane_wypozyczen.id_samochodu = samochody.id_samochodu
+JOIN klasy_samochodow ON klasy_samochodow.id_klasy = samochody.id_klasy
+/*WHERE
+    dane_wypozyczen.cena_doba >= AVG(samochody.cena_katalogowa)*/
+GROUP by klasy_samochodow.nazwa_klasy
+	
+9.      Wyświetl informacje o samochodach, których cena wypożyczenia za dobę jest największa w danej klasie.
+
+
+	
 10.	Wyświetl imiona i nazwiska klientów (imię i nazwisko), którzy wypożyczali auto Opel Astra, ale nigdy nie wypożyczali Forda Mondeo.
 
+SELECT
+    *
+FROM
+    klienci
+WHERE
+    id_klienta IN(
+    SELECT
+        klienci.id_klienta
+    FROM
+        klienci
+    JOIN wypozyczenia ON klienci.id_klienta = wypozyczenia.id_klienta
+    JOIN dane_wypozyczen ON wypozyczenia.id_wypozyczenia = dane_wypozyczen.id_wypozyczenia
+    JOIN samochody ON dane_wypozyczen.id_samochodu = samochody.id_samochodu
+    WHERE
+        samochody.model = "astra"
+) AND id_klienta NOT IN(
+    SELECT
+        klienci.id_klienta
+    FROM
+        klienci
+    JOIN wypozyczenia ON klienci.id_klienta = wypozyczenia.id_klienta
+    JOIN dane_wypozyczen ON wypozyczenia.id_wypozyczenia = dane_wypozyczen.id_wypozyczenia
+    JOIN samochody ON dane_wypozyczen.id_samochodu = samochody.id_samochodu
+    WHERE
+        samochody.model = "mondeo"
+);
+	
+/////////////////////////////////////////////////////////////////////////////
+/* UNION */
+SELECT 
+	klienci.imie_klienta as "imie",
+    klienci.nazwisko_klienta as 'nazw',
+    'klient' as 'kto'
+FROM klienci
 
+UNION
 
+SELECT
+	pracownicy.imie_pracownika,
+    pracownicy.nazwisko_pracownika,
+    'pracownik'
+FROM pracownicy;
 
+////////////////////////////////////////////////////////////////////////////
+/* UNION ALL */
+SELECT 
+	klienci.id_klienta,
+	klienci.miasto_klienta as "miasto"
+FROM klienci
+
+UNION ALL
+
+SELECT
+	pracownicy.id_pracownika,
+	pracownicy.miasto_pracownika
+FROM pracownicy;
+
+///////////////////////////////////////////////////////////////////////////
+/* INTERSECT */
+SELECT 
+	klienci.miasto_klienta as "miasto"
+FROM klienci
+
+intersect
+
+SELECT
+	pracownicy.miasto_pracownika
+FROM pracownicy;
